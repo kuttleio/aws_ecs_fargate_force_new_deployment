5196ba38b0ee9b0b04be12f66a01276f49111912
Dmitriy Solodukha <dspv@me.com>
2022-04-13 18:38:01 +0300

    Combine List & Force lambdas into one process (#6)
    * Combine List & Force lambdas into one process
    * Add schedule

--- variables.tf
+++ variables.tf
@@ -9,20 +9,11 @@ variable ecs_cluster {
     type = string
 }
 
-variable list_rule {
-    description = "Schedule rule for listing service"
+variable schedule {
+    description = "Schedule rule for Lambda"
     default     = {
         name            = "run-every-week"
-        description     = "Fires Lambda every Sunday morning"
+        description     = "Fires Lambda every Sunday morning at 7 UTC"
         expression      = "cron(0 7 ? * SUN *)"
-    }   
-}
-
-variable action_rule {
-    description = "Schedule actions for the listed services"
-    default     = {
-        name            = "run-every-10-min"
-        description     = "Fires Lambda every 10 minutes but only Sundays"
-        expression      = "cron(0/10 * ? * SUN *)"
     }   
 }
